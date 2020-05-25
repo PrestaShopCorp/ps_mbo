2c056a7ea9858cf8918e52bdeae918ba6c486b97
Matt75 <5262628+Matt75@users.noreply.github.com>
2020-05-25 14:48:36 +0200

    Fix for PrestaSop installed in subdirectory

--- ps_mbo.php
+++ ps_mbo.php
@@ -308,7 +308,8 @@ class ps_mbo extends Module
         ) {
             $this->context->controller->addCSS($this->getPathUri() . 'views/css/recommended-modules.css');
             $this->context->controller->addJs(
-                str_ireplace(
+                rtrim(__PS_BASE_URI__, '/')
+                . str_ireplace(
                     _PS_CORE_DIR_,
                     '',
                     _PS_BO_ALL_THEMES_DIR_
