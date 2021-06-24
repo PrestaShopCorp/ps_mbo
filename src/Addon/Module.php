dcee5d9c0a5015558f1072e8f46ca6eac089b0eb
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2021-06-24 19:01:10 +0200

    Fix class namespace

--- src/Addon/Module.php
+++ src/Addon/Module.php
@@ -24,7 +24,7 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-namespace PrestaShop\Module\Mbo\Addon\Module;
+namespace PrestaShop\Module\Mbo\Addon;
 
 use Module as LegacyModule;
 use PrestaShop\Module\Mbo\Addon\AddonListFilterOrigin;
