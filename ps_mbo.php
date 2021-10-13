9ba607fe94ea0b82602514316fd9b1eb41fd5e0a
Vincent Garcia <vgarcia.account@pm.me>
2021-10-13 16:14:56 +0200

    Add dashboardZoneTwo & dashboardZoneThree hooks
    Display the right column on the dashboard.
    Add the "Add more dashboard modules" link at the bottom of the middle column.

--- ps_mbo.php
+++ ps_mbo.php
@@ -27,6 +27,7 @@ if (file_exists($autoloadPath)) {
 }
 use PrestaShop\Module\Mbo\Tab\TabCollectionProvider;
 use PrestaShop\PrestaShop\Adapter\SymfonyContainer;
+use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManagerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
@@ -126,6 +127,8 @@ class ps_mbo extends Module
         'actionAdminControllerSetMedia',
         'displayDashboardTop',
         'displayBackOfficeEmployeeMenu',
+        'dashboardZoneTwo',
+        'dashboardZoneThree',
     ];
 
     /**
@@ -473,6 +476,40 @@ class ps_mbo extends Module
     }
 
     /**
+     * Display addons link on the middle column of the dashboard
+     *
+     * @param  array  $params
+     *
+     * @return false|string
+     */
+    public function hookDashboardZoneTwo(array $params)
+    {
+        return $this->display(__FILE__, 'dashboard-zone-two.tpl');
+    }
+
+    /**
+     * Display addons data & links in the third column of the dashboard
+     *
+     * @param  array  $params
+     *
+     * @return false|string
+     */
+    public function hookDashboardZoneThree(array $params)
+    {
+        $moduleManagerBuilder = ModuleManagerBuilder::getInstance();
+        $moduleManager = $moduleManagerBuilder->build();
+
+        $this->context->smarty->assign(
+            array(
+                'new_version_url' => Tools::getCurrentUrlProtocolPrefix() . _PS_API_DOMAIN_ . '/version/check_version.php?v=' . _PS_VERSION_ . '&lang=' . $this->context->language->iso_code . '&autoupgrade=' . (int) ($moduleManager->isInstalled('autoupgrade') && $moduleManager->isEnabled('autoupgrade')) . '&hosted_mode=' . (int) defined('_PS_HOST_MODE_'),
+                'help_center_link' => $this->getHelpCenterLink($this->context->language->iso_code),
+            )
+        );
+
+        return $this->display(__FILE__, 'dashboard-zone-three.tpl');
+    }
+
+    /**
      * Indicates if the recommended modules should be attached after content in this page
      *
      * @return bool
@@ -510,6 +547,29 @@ class ps_mbo extends Module
         }
 
         return in_array(Tools::getValue('controller'), static::TABS_WITH_RECOMMENDED_MODULES_BUTTON, true);
+    }
+
+    /**
+     * Returns the Help center link for the provided locale
+     *
+     * @param string $languageCode 2-letter locale code
+     *
+     * @return string
+     */
+    private function getHelpCenterLink($languageCode)
+    {
+        $links = [
+            'fr' => 'https://www.prestashop.com/fr/contact?utm_source=back-office&utm_medium=links&utm_campaign=help-center-fr&utm_content=download17',
+            'en' => 'https://www.prestashop.com/en/contact?utm_source=back-office&utm_medium=links&utm_campaign=help-center-en&utm_content=download17',
+            'es' => 'https://www.prestashop.com/es/contacto?utm_source=back-office&utm_medium=links&utm_campaign=help-center-es&utm_content=download17',
+            'de' => 'https://www.prestashop.com/de/kontakt?utm_source=back-office&utm_medium=links&utm_campaign=help-center-de&utm_content=download17',
+            'it' => 'https://www.prestashop.com/it/contatti?utm_source=back-office&utm_medium=links&utm_campaign=help-center-it&utm_content=download17',
+            'nl' => 'https://www.prestashop.com/nl/contacteer-ons?utm_source=back-office&utm_medium=links&utm_campaign=help-center-nl&utm_content=download17',
+            'pt' => 'https://www.prestashop.com/pt/contato?utm_source=back-office&utm_medium=links&utm_campaign=help-center-pt&utm_content=download17',
+            'pl' => 'https://www.prestashop.com/pl/kontakt?utm_source=back-office&utm_medium=links&utm_campaign=help-center-pl&utm_content=download17',
+        ];
+
+        return isset($links[$languageCode]) ? $links[$languageCode] : $links['en'];
     }
 
     /**
