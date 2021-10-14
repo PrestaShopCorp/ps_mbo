f397e4ee570f094eb4603793f94c71c9baa4502e
Vincent Garcia <vgarcia.account@pm.me>
2021-10-14 17:57:06 +0200

    Migrate hooks from ps_mbo to Trait
    Add a trait (UseHooks) to the module src in order to reduce size & complexity of base file.
    Move all hooks declarations in this trait.

--- ps_mbo.php
+++ ps_mbo.php
@@ -26,14 +26,13 @@ if (file_exists($autoloadPath)) {
     require_once $autoloadPath;
 }
 
-use PrestaShop\Module\Mbo\Tab\TabCollectionProvider;
 use PrestaShop\PrestaShop\Adapter\SymfonyContainer;
-use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManagerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ps_mbo extends Module
 {
+    use PrestaShop\Module\Mbo\Traits\UseHooks;
+
     const TABS_WITH_RECOMMENDED_MODULES_BUTTON = [
         'AdminProducts',
         'AdminCategories',
@@ -167,26 +166,34 @@ class ps_mbo extends Module
      *
      * @return bool
      */
-    public function install()
+    public function install(): bool
     {
         return parent::install()
             && $this->registerHook(static::HOOKS);
     }
 
-    public function postInstall()
-    {
-        return true;
-    }
-
     /**
      * Enable Module.
      *
+     * @param bool $force_all
      * @return bool
      */
-    public function enable($force_all = false)
+    public function enable($force_all = false): bool
     {
         return parent::enable($force_all)
             && $this->installTabs();
+    }
+
+    /**
+     * Disable Module.
+     *
+     * @param  bool  $force_all
+     * @return bool
+     */
+    public function disable($force_all = false): bool
+    {
+        return parent::disable($force_all)
+            && $this->uninstallTabs();
     }
 
     /**
@@ -194,7 +201,7 @@ class ps_mbo extends Module
      *
      * @return bool
      */
-    public function installTabs()
+    public function installTabs(): bool
     {
         foreach (static::ADMIN_CONTROLLERS as $adminTab) {
             if (false === $this->installTab($adminTab)) {
@@ -213,7 +220,7 @@ class ps_mbo extends Module
      *
      * @return bool
      */
-    public function installTab(array $tabData)
+    public function installTab(array $tabData): bool
     {
         $position = 0;
         $tabNameByLangId = array_fill_keys(
@@ -224,7 +231,7 @@ class ps_mbo extends Module
         $tab = new Tab();
         $tab->module = $this->name;
         $tab->class_name = $tabData['class_name'];
-        $tab->position = (int) $position;
+        $tab->position = $position;
         $tab->id_parent = empty($tabData['parent_class_name']) ? -1 : Tab::getIdFromClassName($tabData['parent_class_name']);
         $tab->name = $tabNameByLangId;
         $tab->active = true;
@@ -235,7 +242,7 @@ class ps_mbo extends Module
 
         if (Validate::isLoadedObject($tab)) {
             // Updating the id_parent will override the position, that's why we save 2 times
-            $tab->position = (int) $position;
+            $tab->position = $position;
             $tab->save();
         }
 
@@ -243,22 +250,11 @@ class ps_mbo extends Module
     }
 
     /**
-     * Disable Module.
-     *
-     * @return bool
-     */
-    public function disable($force_all = false)
-    {
-        return parent::disable($force_all)
-            && $this->uninstallTabs();
-    }
-
-    /**
      * Uninstall all Tabs.
      *
      * @return bool
      */
-    public function uninstallTabs()
+    public function uninstallTabs(): bool
     {
         foreach (static::ADMIN_CONTROLLERS as $adminTab) {
             if (false === $this->uninstallTab($adminTab)) {
@@ -273,11 +269,11 @@ class ps_mbo extends Module
      * Uninstall Tab.
      * Can be used in upgrade script.
      *
-     * @param array $tabData
+     * @param  array  $tabData
      *
      * @return bool
      */
-    public function uninstallTab(array $tabData)
+    public function uninstallTab(array $tabData): bool
     {
         $tabId = Tab::getIdFromClassName($tabData['class_name']);
         $tab = new Tab($tabId);
@@ -307,270 +303,6 @@ class ps_mbo extends Module
     }
 
     /**
-     * Hook actionAdminControllerSetMedia.
-     */
-    public function hookActionAdminControllerSetMedia()
-    {
-        // has to be loaded in header to prevent flash of content
-        $this->context->controller->addJs($this->getPathUri() . 'views/js/recommended-modules.js?v=' . $this->version);
-
-        if ($this->shouldAttachRecommendedModulesButton()
-            || $this->shouldAttachRecommendedModulesAfterContent()
-        ) {
-            $this->context->controller->addCSS($this->getPathUri() . 'views/css/recommended-modules.css');
-            $this->context->controller->addJs(
-                rtrim(__PS_BASE_URI__, '/')
-                . str_ireplace(
-                    _PS_CORE_DIR_,
-                    '',
-                    _PS_BO_ALL_THEMES_DIR_
-                )
-                . 'default/js/bundle/module/module_card.js?v='
-                . _PS_VERSION_
-            );
-        }
-    }
-
-    /**
-     * Hook displayDashboardTop.
-     * Includes content just below the toolbar.
-     *
-     * @return string
-     */
-    public function hookDisplayDashboardTop()
-    {
-        /** @var UrlGeneratorInterface $router */
-        $router = $this->get('router');
-
-        try {
-            $recommendedModulesUrl = $router->generate(
-                'admin_mbo_recommended_modules',
-                [
-                    'tabClassName' => Tools::getValue('controller'),
-                ]
-            );
-        } catch (Exception $exception) {
-            // Avoid fatal errors on ServiceNotFoundException
-            return '';
-        }
-
-        $this->smarty->assign([
-            'shouldAttachRecommendedModulesAfterContent' => $this->shouldAttachRecommendedModulesAfterContent(),
-            'shouldAttachRecommendedModulesButton' => $this->shouldAttachRecommendedModulesButton(),
-            'shouldUseLegacyTheme' => $this->isAdminLegacyContext(),
-            'recommendedModulesTitleTranslated' => $this->trans('Recommended Modules and Services'),
-            'recommendedModulesCloseTranslated' => $this->trans('Close', [], 'Admin.Actions'),
-            'recommendedModulesUrl' => $recommendedModulesUrl,
-        ]);
-
-        return $this->fetch('module:ps_mbo/views/templates/hook/recommended-modules.tpl');
-    }
-
-    /**
-     * Hook displayBackOfficeMenu.
-     * Returns menu in BackOffice
-     *
-     * @param array $params
-     *
-     * @return void
-     */
-    public function hookDisplayBackOfficeEmployeeMenu(array $params)
-    {
-        if (!class_exists(\PrestaShopBundle\Component\ActionBar\ActionsBarButtonsCollection::class)
-            || !class_exists(\PrestaShopBundle\Component\ActionBar\ActionsBarButton::class)
-            || !($params['links'] instanceof \PrestaShopBundle\Component\ActionBar\ActionsBarButtonsCollection)) {
-            return;
-        }
-        $params['links']->add(
-            new \PrestaShopBundle\Component\ActionBar\ActionsBarButton(
-                __CLASS__,
-                [
-                    'link' => $this->trans(
-                        'https://www.prestashop.com/en/resources/documentations?utm_source=back-office&utm_medium=profile&utm_campaign=resources-en&utm_content=download17',
-                        [],
-                        'Admin.Navigation.Header'
-                    ),
-                    'icon' => 'book',
-                ],
-                $this->trans(
-                    'Resources',
-                    [],
-                    'Admin.Navigation.Header'
-                )
-            )
-        );
-        $params['links']->add(
-            new \PrestaShopBundle\Component\ActionBar\ActionsBarButton(
-                __CLASS__,
-                [
-                    'link' => $this->trans(
-                        'https://www.prestashop.com/en/training?utm_source=back-office&utm_medium=profile&utm_campaign=training-en&utm_content=download17',
-                        [],
-                        'Admin.Navigation.Header'
-                    ),
-                    'icon' => 'school',
-                ],
-                $this->trans(
-                    'Training',
-                    [],
-                    'Admin.Navigation.Header'
-                )
-            )
-        );
-        $params['links']->add(
-            new \PrestaShopBundle\Component\ActionBar\ActionsBarButton(
-                __CLASS__,
-                [
-                    'link' => $this->trans(
-                        'https://www.prestashop.com/en/experts?utm_source=back-office&utm_medium=profile&utm_campaign=expert-en&utm_content=download17',
-                        [],
-                        'Admin.Navigation.Header'
-                    ),
-                    'icon' => 'person_pin_circle',
-                ],
-                $this->trans(
-                    'Find an Expert',
-                    [],
-                    'Admin.Navigation.Header'
-                )
-            )
-        );
-        $params['links']->add(
-            new \PrestaShopBundle\Component\ActionBar\ActionsBarButton(
-                __CLASS__,
-                [
-                    'link' => $this->trans(
-                        'https://addons.prestashop.com?utm_source=back-office&utm_medium=profile&utm_campaign=addons-en&utm_content=download17',
-                        [],
-                        'Admin.Navigation.Header'
-                    ),
-                    'icon' => 'extension',
-                ],
-                $this->trans(
-                    'PrestaShop Marketplace',
-                    [],
-                    'Admin.Navigation.Header'
-                )
-            )
-        );
-        $params['links']->add(
-            new \PrestaShopBundle\Component\ActionBar\ActionsBarButton(
-                __CLASS__,
-                [
-                    'link' => $this->trans(
-                        'https://www.prestashop.com/en/contact?utm_source=back-office&utm_medium=profile&utm_campaign=help-center-en&utm_content=download17',
-                        [],
-                        'Admin.Navigation.Header'
-                    ),
-                    'icon' => 'help',
-                ],
-                $this->trans(
-                    'Help Center',
-                    [],
-                    'Admin.Navigation.Header'
-                )
-            )
-        );
-    }
-
-    /**
-     * Display addons link on the middle column of the dashboard
-     *
-     * @param array $params
-     *
-     * @return false|string
-     */
-    public function hookDashboardZoneTwo(array $params)
-    {
-        return $this->display(__FILE__, 'dashboard-zone-two.tpl');
-    }
-
-    /**
-     * Display addons data & links in the third column of the dashboard
-     *
-     * @param array $params
-     *
-     * @return false|string
-     */
-    public function hookDashboardZoneThree(array $params)
-    {
-        $moduleManagerBuilder = ModuleManagerBuilder::getInstance();
-        $moduleManager = $moduleManagerBuilder->build();
-
-        $this->context->smarty->assign(
-            [
-                'new_version_url' => Tools::getCurrentUrlProtocolPrefix() . _PS_API_DOMAIN_ . '/version/check_version.php?v=' . _PS_VERSION_ . '&lang=' . $this->context->language->iso_code . '&autoupgrade=' . (int) ($moduleManager->isInstalled('autoupgrade') && $moduleManager->isEnabled('autoupgrade')) . '&hosted_mode=' . (int) defined('_PS_HOST_MODE_'),
-                'help_center_link' => $this->getHelpCenterLink($this->context->language->iso_code),
-            ]
-        );
-
-        return $this->display(__FILE__, 'dashboard-zone-three.tpl');
-    }
-
-    /**
-     * Indicates if the recommended modules should be attached after content in this page
-     *
-     * @return bool
-     */
-    private function shouldAttachRecommendedModulesAfterContent()
-    {
-        // AdminLogin should not call TabCollectionProvider
-        if (Validate::isLoadedObject($this->context->employee)) {
-            /** @var TabCollectionProvider $tabCollectionProvider */
-            $tabCollectionProvider = $this->get('mbo.tab.collection.provider');
-            if ($tabCollectionProvider->isTabCollectionCached()) {
-                return $tabCollectionProvider->getTabCollection()->getTab(Tools::getValue('controller'))->shouldDisplayAfterContent()
-                    || 'AdminCarriers' === Tools::getValue('controller');
-            }
-        }
-
-        return in_array(Tools::getValue('controller'), static::TABS_WITH_RECOMMENDED_MODULES_AFTER_CONTENT, true);
-    }
-
-    /**
-     * Indicates if the recommended modules button should be attached in this page
-     *
-     * @return bool
-     */
-    private function shouldAttachRecommendedModulesButton()
-    {
-        // AdminLogin should not call TabCollectionProvider
-        if (Validate::isLoadedObject($this->context->employee)) {
-            /** @var TabCollectionProvider $tabCollectionProvider */
-            $tabCollectionProvider = $this->get('mbo.tab.collection.provider');
-            if ($tabCollectionProvider->isTabCollectionCached()) {
-                return $tabCollectionProvider->getTabCollection()->getTab(Tools::getValue('controller'))->shouldDisplayButton()
-                    && 'AdminCarriers' !== Tools::getValue('controller');
-            }
-        }
-
-        return in_array(Tools::getValue('controller'), static::TABS_WITH_RECOMMENDED_MODULES_BUTTON, true);
-    }
-
-    /**
-     * Returns the Help center link for the provided locale
-     *
-     * @param string $languageCode 2-letter locale code
-     *
-     * @return string
-     */
-    private function getHelpCenterLink($languageCode)
-    {
-        $links = [
-            'fr' => 'https://www.prestashop.com/fr/contact?utm_source=back-office&utm_medium=links&utm_campaign=help-center-fr&utm_content=download17',
-            'en' => 'https://www.prestashop.com/en/contact?utm_source=back-office&utm_medium=links&utm_campaign=help-center-en&utm_content=download17',
-            'es' => 'https://www.prestashop.com/es/contacto?utm_source=back-office&utm_medium=links&utm_campaign=help-center-es&utm_content=download17',
-            'de' => 'https://www.prestashop.com/de/kontakt?utm_source=back-office&utm_medium=links&utm_campaign=help-center-de&utm_content=download17',
-            'it' => 'https://www.prestashop.com/it/contatti?utm_source=back-office&utm_medium=links&utm_campaign=help-center-it&utm_content=download17',
-            'nl' => 'https://www.prestashop.com/nl/contacteer-ons?utm_source=back-office&utm_medium=links&utm_campaign=help-center-nl&utm_content=download17',
-            'pt' => 'https://www.prestashop.com/pt/contato?utm_source=back-office&utm_medium=links&utm_campaign=help-center-pt&utm_content=download17',
-            'pl' => 'https://www.prestashop.com/pl/kontakt?utm_source=back-office&utm_medium=links&utm_campaign=help-center-pl&utm_content=download17',
-        ];
-
-        return isset($links[$languageCode]) ? $links[$languageCode] : $links['en'];
-    }
-
-    /**
      * Override of native function to always retrieve Symfony container instead of legacy admin container on legacy context.
      *
      * {@inheritdoc}
@@ -582,20 +314,5 @@ class ps_mbo extends Module
         }
 
         return $this->container->get($serviceName);
-    }
-
-    protected function getAddonsModules()
-    {
-        $addons_modules = [];
-        $content = Tools::addonsRequest('install-modules', []);
-        $xml = @simplexml_load_string($content, 'SimpleXMLElement', LIBXML_NOCDATA);
-
-        if ($xml !== false && isset($xml->module)) {
-            foreach ($xml->module as $modaddons) {
-                $addons_modules[] = ['id_module' => $modaddons->id, 'name' => $modaddons->name];
-            }
-        }
-
-        return $addons_modules;
     }
 }
---
+++ src/Traits/UseHooks.php
@@ -0,0 +1,225 @@
+<?php
+/**
+ * 2007-2020 PrestaShop and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+namespace PrestaShop\Module\Mbo\Traits;
+
+use ToolsCore as Tools;
+use ValidateCore as Validate;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManagerBuilder;
+use PrestaShop\Module\Mbo\Tab\TabCollectionProvider;
+
+trait UseHooks
+{
+    /**
+     * Hook displayDashboardTop.
+     * Includes content just below the toolbar.
+     *
+     * @return string
+     */
+    public function hookDisplayDashboardTop(): string
+    {
+        /** @var UrlGeneratorInterface $router */
+        $router = $this->get('router');
+
+        try {
+            $recommendedModulesUrl = $router->generate(
+                'admin_mbo_recommended_modules',
+                [
+                    'tabClassName' => Tools::getValue('controller'),
+                ]
+            );
+        } catch (\Exception $exception) {
+            // Avoid fatal errors on ServiceNotFoundException
+            return '';
+        }
+
+        $this->smarty->assign([
+            'shouldAttachRecommendedModulesAfterContent' => $this->shouldAttachRecommendedModules(static::TABS_WITH_RECOMMENDED_MODULES_AFTER_CONTENT),
+            'shouldAttachRecommendedModulesButton' => $this->shouldAttachRecommendedModules(static::TABS_WITH_RECOMMENDED_MODULES_BUTTON),
+            'shouldUseLegacyTheme' => $this->isAdminLegacyContext(),
+            'recommendedModulesTitleTranslated' => $this->trans('Recommended Modules and Services'),
+            'recommendedModulesCloseTranslated' => $this->trans('Close', [], 'Admin.Actions'),
+            'recommendedModulesUrl' => $recommendedModulesUrl,
+        ]);
+
+        return $this->fetch('module:ps_mbo/views/templates/hook/recommended-modules.tpl');
+    }
+
+    /**
+     * Display addons link on the middle column of the dashboard
+     *
+     * @param  array  $params
+     *
+     * @return false|string
+     */
+    public function hookDashboardZoneTwo(array $params)
+    {
+        return $this->display($this->name, 'dashboard-zone-two.tpl');
+    }
+
+    /**
+     * Display addons data & links in the third column of the dashboard
+     *
+     * @param  array  $params
+     *
+     * @return false|string
+     */
+    public function hookDashboardZoneThree(array $params)
+    {
+        $moduleManagerBuilder = ModuleManagerBuilder::getInstance();
+        $moduleManager = $moduleManagerBuilder->build();
+
+        $this->context->smarty->assign(
+            [
+                'new_version_url' => Tools::getCurrentUrlProtocolPrefix()._PS_API_DOMAIN_.'/version/check_version.php?v='._PS_VERSION_.'&lang='.$this->context->language->iso_code.'&autoupgrade='.(int)($moduleManager->isInstalled('autoupgrade') && $moduleManager->isEnabled('autoupgrade')).'&hosted_mode='.(int)defined('_PS_HOST_MODE_'),
+                'help_center_link' => $this->getHelpCenterLink($this->context->language->iso_code),
+            ]
+        );
+
+        return $this->display($this->name, 'dashboard-zone-three.tpl');
+    }
+
+    /**
+     * Hook displayBackOfficeMenu.
+     * Returns menu in BackOffice
+     *
+     * @param  array  $params
+     *
+     * @return void
+     */
+    public function hookDisplayBackOfficeEmployeeMenu(array $params): void
+    {
+        if (!class_exists(\PrestaShopBundle\Component\ActionBar\ActionsBarButtonsCollection::class)
+            || !class_exists(\PrestaShopBundle\Component\ActionBar\ActionsBarButton::class)
+            || !($params['links'] instanceof \PrestaShopBundle\Component\ActionBar\ActionsBarButtonsCollection)) {
+            return;
+        }
+
+        $links = [
+            [
+                'url' => 'https://www.prestashop.com/en/resources/documentations?utm_source=back-office&utm_medium=profile&utm_campaign=resources-en&utm_content=download17',
+                'icon' => 'book',
+                'label' => 'Resources',
+            ],
+            [
+                'url' => 'https://www.prestashop.com/en/training?utm_source=back-office&utm_medium=profile&utm_campaign=training-en&utm_content=download17',
+                'icon' => 'school',
+                'label' => 'Training',
+            ],
+            [
+                'url' => 'https://www.prestashop.com/en/experts?utm_source=back-office&utm_medium=profile&utm_campaign=expert-en&utm_content=download17',
+                'icon' => 'person_pin_circle',
+                'label' => 'Find an Expert',
+            ],
+            [
+                'url' => 'https://addons.prestashop.com?utm_source=back-office&utm_medium=profile&utm_campaign=addons-en&utm_content=download17',
+                'icon' => 'extension',
+                'label' => 'PrestaShop Marketplace',
+            ],
+            [
+                'url' => 'https://www.prestashop.com/en/contact?utm_source=back-office&utm_medium=profile&utm_campaign=help-center-en&utm_content=download17',
+                'icon' => 'help',
+                'label' => 'Help Center',
+            ],
+        ];
+
+        foreach ($links as $link) {
+            $params['links']->add(
+                new \PrestaShopBundle\Component\ActionBar\ActionsBarButton(
+                    __CLASS__,
+                    [
+                        'link' => $this->trans($link['url'], [], 'Admin.Navigation.Header'),
+                        'icon' => $link['icon'],
+                    ],
+                    $this->trans($link['label'], [], 'Admin.Navigation.Header')
+                )
+            );
+        }
+    }
+
+    /**
+     * Hook actionAdminControllerSetMedia.
+     */
+    public function hookActionAdminControllerSetMedia(): void
+    {
+        // has to be loaded in header to prevent flash of content
+        $this->context->controller->addJs($this->getPathUri() . 'views/js/recommended-modules.js?v=' . $this->version);
+
+        if ($this->shouldAttachRecommendedModules(static::TABS_WITH_RECOMMENDED_MODULES_BUTTON)
+            || $this->shouldAttachRecommendedModules(static::TABS_WITH_RECOMMENDED_MODULES_AFTER_CONTENT)
+        ) {
+            $this->context->controller->addCSS($this->getPathUri() . 'views/css/recommended-modules.css');
+            $this->context->controller->addJs(
+                rtrim(__PS_BASE_URI__, '/')
+                . str_ireplace(
+                    _PS_CORE_DIR_,
+                    '',
+                    _PS_BO_ALL_THEMES_DIR_
+                )
+                . 'default/js/bundle/module/module_card.js?v='
+                . _PS_VERSION_
+            );
+        }
+    }
+
+    /**
+     * Indicates if the recommended modules should be attached
+     *
+     * @param array $modules
+     * @return bool
+     */
+    private function shouldAttachRecommendedModules(array $modules): bool
+    {
+        // AdminLogin should not call TabCollectionProvider
+        if (Validate::isLoadedObject($this->context->employee)) {
+            /** @var TabCollectionProvider $tabCollectionProvider */
+            $tabCollectionProvider = $this->get('mbo.tab.collection.provider');
+            if ($tabCollectionProvider->isTabCollectionCached()) {
+                return $tabCollectionProvider->getTabCollection()->getTab(Tools::getValue('controller'))->shouldDisplayAfterContent()
+                    || 'AdminCarriers' === Tools::getValue('controller');
+            }
+        }
+
+        return in_array(Tools::getValue('controller'), $modules, true);
+    }
+
+    /**
+     * Returns the Help center link for the provided locale
+     *
+     * @param  string  $languageCode  2-letter locale code
+     *
+     * @return string
+     */
+    private function getHelpCenterLink(string $languageCode): string
+    {
+        $links = [
+            'fr' => 'https://www.prestashop.com/fr/contact?utm_source=back-office&utm_medium=links&utm_campaign=help-center-fr&utm_content=download17',
+            'en' => 'https://www.prestashop.com/en/contact?utm_source=back-office&utm_medium=links&utm_campaign=help-center-en&utm_content=download17',
+            'es' => 'https://www.prestashop.com/es/contacto?utm_source=back-office&utm_medium=links&utm_campaign=help-center-es&utm_content=download17',
+            'de' => 'https://www.prestashop.com/de/kontakt?utm_source=back-office&utm_medium=links&utm_campaign=help-center-de&utm_content=download17',
+            'it' => 'https://www.prestashop.com/it/contatti?utm_source=back-office&utm_medium=links&utm_campaign=help-center-it&utm_content=download17',
+            'nl' => 'https://www.prestashop.com/nl/contacteer-ons?utm_source=back-office&utm_medium=links&utm_campaign=help-center-nl&utm_content=download17',
+            'pt' => 'https://www.prestashop.com/pt/contato?utm_source=back-office&utm_medium=links&utm_campaign=help-center-pt&utm_content=download17',
+            'pl' => 'https://www.prestashop.com/pl/kontakt?utm_source=back-office&utm_medium=links&utm_campaign=help-center-pl&utm_content=download17',
+        ];
+
+        return $links[$languageCode] ?? $links['en'];
+    }
+}
