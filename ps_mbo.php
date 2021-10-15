6fe860cacc7c076472db91d776ae44591b31caac
Vincent Garcia <vgarcia.account@pm.me>
2021-10-15 17:42:13 +0200

    Move Tabs management in a trait

--- ps_mbo.php
+++ ps_mbo.php
@@ -32,47 +32,15 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ps_mbo extends Module
 {
     use PrestaShop\Module\Mbo\Traits\UseHooks;
+    use PrestaShop\Module\Mbo\Traits\HaveTabs;
 
-    const ADMIN_CONTROLLERS = [
-        'AdminPsMboModuleParent' => [
-            'name' => 'Module catalog',
-            'visible' => true,
-            'class_name' => 'AdminPsMboModuleParent',
-            'parent_class_name' => 'AdminParentModulesSf',
-        ],
-        'AdminPsMboModule' => [
-            'name' => 'Module catalog',
-            'visible' => true,
-            'class_name' => 'AdminPsMboModule',
-            'parent_class_name' => 'AdminPsMboModuleParent',
-        ],
-        'AdminPsMboSelection' => [
-            'name' => 'Module selection',
-            'visible' => true,
-            'class_name' => 'AdminPsMboSelection',
-            'parent_class_name' => 'AdminPsMboModuleParent',
-        ],
-        'AdminPsMboRecommended' => [
-            'name' => 'Module recommended',
-            'visible' => true,
-            'class_name' => 'AdminPsMboRecommended',
-        ],
-        'AdminPsMboTheme' => [
-            'name' => 'Theme catalog',
-            'visible' => true,
-            'class_name' => 'AdminPsMboTheme',
-            'parent_class_name' => 'AdminParentThemes',
-        ],
-    ];
-
-    const HOOKS = [
+    public const HOOKS = [
         'actionAdminControllerSetMedia',
         'displayDashboardTop',
         'displayBackOfficeEmployeeMenu',
         'dashboardZoneTwo',
         'dashboardZoneThree',
     ];
-
     /**
      * @var ContainerInterface
      */
@@ -121,7 +89,7 @@ class ps_mbo extends Module
     public function enable($force_all = false): bool
     {
         return parent::enable($force_all)
-            && $this->installTabs();
+            && $this->handleTabActions('install');
     }
 
     /**
@@ -134,113 +102,7 @@ class ps_mbo extends Module
     public function disable($force_all = false): bool
     {
         return parent::disable($force_all)
-            && $this->uninstallTabs();
-    }
-
-    /**
-     * Install all Tabs.
-     *
-     * @return bool
-     */
-    public function installTabs(): bool
-    {
-        foreach (static::ADMIN_CONTROLLERS as $adminTab) {
-            if (false === $this->installTab($adminTab)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Install Tab.
-     * Used in upgrade script.
-     *
-     * @param array $tabData
-     *
-     * @return bool
-     */
-    public function installTab(array $tabData): bool
-    {
-        $position = 0;
-        $tabNameByLangId = array_fill_keys(
-            Language::getIDs(false),
-            $tabData['name']
-        );
-
-        $tab = new Tab();
-        $tab->module = $this->name;
-        $tab->class_name = $tabData['class_name'];
-        $tab->position = $position;
-        $tab->id_parent = empty($tabData['parent_class_name']) ? -1 : Tab::getIdFromClassName($tabData['parent_class_name']);
-        $tab->name = $tabNameByLangId;
-        $tab->active = true;
-
-        if (false === (bool) $tab->add()) {
-            return false;
-        }
-
-        if (Validate::isLoadedObject($tab)) {
-            // Updating the id_parent will override the position, that's why we save 2 times
-            $tab->position = $position;
-            $tab->save();
-        }
-
-        return true;
-    }
-
-    /**
-     * Uninstall all Tabs.
-     *
-     * @return bool
-     */
-    public function uninstallTabs(): bool
-    {
-        foreach (static::ADMIN_CONTROLLERS as $adminTab) {
-            if (false === $this->uninstallTab($adminTab)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Uninstall Tab.
-     * Can be used in upgrade script.
-     *
-     * @param array $tabData
-     *
-     * @return bool
-     */
-    public function uninstallTab(array $tabData): bool
-    {
-        $tabId = Tab::getIdFromClassName($tabData['class_name']);
-        $tab = new Tab($tabId);
-
-        if (false === Validate::isLoadedObject($tab)) {
-            return false;
-        }
-
-        if (false === (bool) $tab->delete()) {
-            return false;
-        }
-
-        if (isset($tabData['core_reference'])) {
-            $tabCoreId = Tab::getIdFromClassName($tabData['core_reference']);
-            $tabCore = new Tab($tabCoreId);
-
-            if (Validate::isLoadedObject($tabCore)) {
-                $tabCore->active = true;
-            }
-
-            if (false === (bool) $tabCore->save()) {
-                return false;
-            }
-        }
-
-        return true;
+            && $this->handleTabActions('uninstall');
     }
 
     /**
