2f1b76c1104b2c6e22bbb9084be7615609531b28
Ibrahima SOW <ibrahima.sow@prestashop.com>
2022-04-29 08:15:33 +0000

    Fix namespace

--- src/Traits/Hooks/UseDisplayBackOfficeEmployeeMenu.php
+++ src/Traits/Hooks/UseDisplayBackOfficeEmployeeMenu.php
@@ -22,8 +22,8 @@ declare(strict_types=1);
 namespace PrestaShop\Module\Mbo\Traits\Hooks;
 
 use PrestaShop\Module\Mbo\Addons\Provider\LinksProvider;
-use PrestaShop\PrestaShop\Core\ActionBar\ActionsBarButton;
-use PrestaShop\PrestaShop\Core\ActionBar\ActionsBarButtonsCollection;
+use PrestaShop\PrestaShop\Core\Action\ActionsBarButton;
+use PrestaShop\PrestaShop\Core\Action\ActionsBarButtonsCollection;
 
 trait UseDisplayBackOfficeEmployeeMenu
 {
