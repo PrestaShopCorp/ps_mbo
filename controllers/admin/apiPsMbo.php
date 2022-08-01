8b019f2bc756950c70b735ee27c940b34f734174
Ibrahima SOW <ibrahima.sow@prestashop.com>
2022-08-01 09:48:33 +0000

    Update token in config url

--- controllers/admin/apiPsMbo.php
+++ controllers/admin/apiPsMbo.php
@@ -32,12 +32,46 @@ class apiPsMboController extends AbstractAdminApiController
         }
 
         $moduleUrls = $module->get('urls');
+        $configUrl = (bool) $module->get('is_configurable') && isset($moduleUrls['configure']) ? $moduleUrls['configure'] : null;
+        $configUrl = $this->generateTokenizedModuleActionUrl($configUrl);
 
         $this->exitWithResponse([
             'message' => $this->trans('Transition successfully executed'),
             'module_status' => $module->getStatus(),
             'version' => $module->get('version'),
-            'config_url' => (bool) $module->get('is_configurable') && isset($moduleUrls['configure']) ? $moduleUrls['configure'] : null,
+            'config_url' => $configUrl,
         ]);
+    }
+
+    private function generateTokenizedModuleActionUrl($url)
+    {
+        $userId = (int) Tools::getValue('user_id');
+
+        $user = new Employee($userId);
+
+        $newToken = $this->get('security.csrf.token_manager')
+            ->getToken($user->email)
+            ->getValue();
+
+//        $token = Tools::getAdminToken('AdminModules' . (int) Tab::getIdFromClassName('AdminModules') . (int) $userId);
+
+        $components = parse_url($url);
+        $baseUrl = (isset($components['path']) ? $components['path'] : '');
+        $queryParams = [];
+        if (isset($components['query'])) {
+            $query = $components['query'];
+
+            parse_str($query, $queryParams);
+        }
+
+        $queryParams['_token'] = $newToken;
+
+        $url = $baseUrl . '?' . http_build_query($queryParams, '', '&');
+        if (isset($components['fragment']) && $components['fragment'] !== '') {
+            /* This copy-paste from Symfony's UrlGenerator */
+            $url .= '#' . strtr(rawurlencode($components['fragment']), ['%2F' => '/', '%3F' => '?']);
+        }
+
+        return $url;
     }
 }
