5072f0c0f38d2fcab437d308520ec5d8550e5afd
Ibrahima SOW <ibrahima.sow@prestashop.com>
2022-09-13 11:56:56 +0000

    Restore code for Module manager modules see-more

--- src/Traits/Hooks/UseActionAdminControllerSetMedia.php
+++ src/Traits/Hooks/UseActionAdminControllerSetMedia.php
@@ -86,6 +86,7 @@ trait UseActionAdminControllerSetMedia
             Tools::getValue('controller') === 'AdminPsMboModule' ||
             Tools::getValue('controller') === 'AdminModulesManage'
         ) {
+            $this->context->controller->addJs($this->getPathUri() . 'views/js/catalog-see-more.js?v=' . $this->version);
             $this->context->controller->addJs('/js/jquery/plugins/growl/jquery.growl.js?v=' . $this->version);
             $this->context->controller->addCSS($this->getPathUri() . 'views/css/module-catalog.css');
         }
