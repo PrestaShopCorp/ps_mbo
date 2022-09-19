50f2f10fe985ab5c9e2b14050944510404cde027
Ibrahima SOW <ibrahima.sow@prestashop.com>
2022-09-19 10:15:29 +0000

    Restore loading of recommended modals stylesheet

--- src/Traits/Hooks/UseDisplayDashboardTop.php
+++ src/Traits/Hooks/UseDisplayDashboardTop.php
@@ -231,5 +231,11 @@ trait UseDisplayDashboardTop
     {
         // has to be loaded in header to prevent flash of content
         $this->context->controller->addJs($this->getPathUri() . 'views/js/recommended-modules.js?v=' . $this->version);
+
+        if ($this->shouldAttachRecommendedModules(TabInterface::RECOMMENDED_BUTTON_TYPE)
+            || $this->shouldAttachRecommendedModules(TabInterface::RECOMMENDED_AFTER_CONTENT_TYPE)
+        ) {
+            $this->context->controller->addCSS($this->getPathUri() . 'views/css/recommended-modules.css');
+        }
     }
 }
