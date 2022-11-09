fd2c138690e8c0740df66dbd80a5a6f1ac3b80ce
Vincent Garcia <vgarcia.account@pm.me>
2022-11-09 17:56:52 +0100

    Feat :page_facing_up: Update license

--- controllers/admin/apiSecurityPsMbo.php
+++ controllers/admin/apiSecurityPsMbo.php
@@ -17,6 +17,7 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
  */
+
 use PrestaShop\Module\Mbo\Api\Config\Config;
 use PrestaShop\Module\Mbo\Api\Controller\AbstractAdminApiController;
 
