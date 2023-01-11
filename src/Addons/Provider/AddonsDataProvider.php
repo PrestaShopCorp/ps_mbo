5121d80cb3c318e8b22c91ba52a6603ecce628e3
Sullivan <sullivan.monteiro@prestashop.com>
2023-01-11 10:17:17 +0100

    Add button on user's connection on accounts

--- src/Addons/Provider/AddonsDataProvider.php
+++ src/Addons/Provider/AddonsDataProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright since 2007 PrestaShop SA and Contributors
  * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
@@ -17,6 +18,7 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
  */
+
 declare(strict_types=1);
 
 namespace PrestaShop\Module\Mbo\Addons\Provider;
