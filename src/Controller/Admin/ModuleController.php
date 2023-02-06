f46ca7c0a6ddfcae199373f10d53433b440a4aec
Julien Rabin <julien.rabin@prestashop.com>
2023-02-06 14:40:15 +0100

    add type manualy for

--- src/Controller/Admin/ModuleController.php
+++ src/Controller/Admin/ModuleController.php
@@ -56,6 +56,10 @@ class ModuleController extends ModuleControllerCore
             return $deniedAccess;
         }
 
+        /**
+         * 
+         * @var AdminModuleDataProvider $modulesProvider
+         */
         $modulesProvider = $this->get('prestashop.core.admin.data_provider.module_interface');
         $moduleRepository = $this->get('prestashop.core.admin.module.repository');
         $responseArray = [];
