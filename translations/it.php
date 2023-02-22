e3802da4134f49a92b6f579cf7611d3f15ab24dc
Sullivan <sullivan.monteiro@prestashop.com>
2023-02-22 16:29:03 +0100

    fix: italian translation

--- translations/it.php
+++ translations/it.php
@@ -3,32 +3,32 @@
 global $_MODULE;
 $_MODULE = [];
 
-$_MODULE['<{mbo}prestashop>ps_mbo_c15784789b60bab84ad3108dacfb3f18'] = 'Trovate i moduli per far crescere la vostra attività';
-$_MODULE['<{mbo}prestashop>ps_mbo_47b2395a25658f70813eb6a7a07f55c6'] = 'Esplorate il Marketplace nel vostro back office per trovare moduli partner gratuiti e a pagamento per far crescere la vostra attività. Aumentare il traffico, migliorare le conversioni o fidelizzare i clienti... c\'è un modulo per ogni esigenza.';
-$_MODULE['<{mbo}prestashop>ps_mbo_57ef9bf513ef4b2e8c3bacd4b4ef00f1'] = 'E se volete dare un nuovo look al vostro negozio, {link}consultate il catalogo dei temi{/link}.';
-$_MODULE['<{mbo}prestashop>ps_mbo_be039e2a2280aba57d733643984b6036'] = '%nbModules% moduli partner compatibili con il vostro negozio';
-$_MODULE['<{mbo}prestashop>ps_mbo_e330c9c975994dd814fb0432aa594588'] = 'Nel back office, vi mostriamo solo i moduli sviluppati da PrestaShop o dai nostri partner che sono compatibili con il vostro negozio.';
-$_MODULE['<{mbo}prestashop>ps_mbo_abfc3a65538a6ec86502b2b498b6b4a6'] = 'Scopri';
-$_MODULE['<{mbo}prestashop>ps_mbo_d53907d724bf05eff15a2e80205f2d1e'] = 'Mostra dettagli';
-$_MODULE['<{mbo}prestashop>ps_mbo_b24ce0cd392a5b0b8dedc66c25213594'] = 'Gratis';
-$_MODULE['<{mbo}prestashop>ps_mbo_70fd3879bc512e7d2048692d397fc3a0'] = 'Moduli in primo piano';
+$_MODULE['<{mbo}prestashop>modulescatalog_c15784789b60bab84ad3108dacfb3f18'] = 'Trovate i moduli per far crescere la vostra attività';
+$_MODULE['<{mbo}prestashop>modulescatalog_47b2395a25658f70813eb6a7a07f55c6'] = 'Esplorate il Marketplace nel vostro back office per trovare moduli partner gratuiti e a pagamento per far crescere la vostra attività. Aumentare il traffico, migliorare le conversioni o fidelizzare i clienti... c\'è un modulo per ogni esigenza.';
+$_MODULE['<{mbo}prestashop>modulescatalog_57ef9bf513ef4b2e8c3bacd4b4ef00f1'] = 'E se volete dare un nuovo look al vostro negozio, {link}consultate il catalogo dei temi{/link}.';
+$_MODULE['<{mbo}prestashop>modulescatalog_be039e2a2280aba57d733643984b6036'] = '%nbModules% moduli partner compatibili con il vostro negozio';
+$_MODULE['<{mbo}prestashop>modulescatalog_e330c9c975994dd814fb0432aa594588'] = 'Nel back office, vi mostriamo solo i moduli sviluppati da PrestaShop o dai nostri partner che sono compatibili con il vostro negozio.';
+$_MODULE['<{mbo}prestashop>modulescatalog_abfc3a65538a6ec86502b2b498b6b4a6'] = 'Scopri';
+$_MODULE['<{mbo}prestashop>modulescatalog_d53907d724bf05eff15a2e80205f2d1e'] = 'Mostra dettagli';
+$_MODULE['<{mbo}prestashop>modulescatalog_b24ce0cd392a5b0b8dedc66c25213594'] = 'Gratis';
+$_MODULE['<{mbo}prestashop>modulescatalog_70fd3879bc512e7d2048692d397fc3a0'] = 'Moduli in primo piano';
 $_MODULE['<{mbo}prestashop>recommendedmodulesandservices_8c883385a642028c9366bb087618b216'] = 'Aumentare le vendite';
 $_MODULE['<{mbo}prestashop>recommendedmodulesandservices_1d929590b318c7d4746d15f1fdc19c81'] = 'Ottimizzare il catalogo prodotti';
 $_MODULE['<{mbo}prestashop>recommendedmodulesandservices_0b63003090cd7fae050a51da250bb0dc'] = 'Migliorare la strategia dei dati';
 $_MODULE['<{mbo}prestashop>recommendedmodulesandservices_3015e7fd48efb2220c184c49eaeae2c1'] = 'Migliorare l\'esperienza del cliente';
 $_MODULE['<{mbo}prestashop>recommendedmodulesandservices_e39767bb8e247250896230e1d6ee036f'] = 'Moduli raccomandati';
-$_MODULE['<{mbo}prestashop>recommendedmodulesandservices_4b89fd5e12aede952bf0b6e47d97733c'] = 'Ottenere nuovi clienti e farli tornare.<br>
+$_MODULE['<{mbo}prestashop>recommendedmodulesandservices_947d8544b50da92594fc6c5ed87af81b'] = 'Ottenere nuovi clienti e farli tornare.<br>
                 Ecco una selezione di moduli partner,<strong> compatibili con il vostro negozio </strong>, per aiutarvi a raggiungere i vostri obiettivi.';
-$_MODULE['<{mbo}prestashop>recommendedmodulesandservices_8f09647a1a90a3272653803298accd6d'] = 'Rendete visibili i vostri prodotti in più e create pagine di prodotto che convertono.<br>
+$_MODULE['<{mbo}prestashop>recommendedmodulesandservices_88b4c622af5f96f61b3cee5fcb49791b'] = 'Rendete visibili i vostri prodotti in più e create pagine di prodotto che convertono.<br>
                 Ecco una selezione di moduli partner,<strong> compatibili con il vostro negozio </strong>, per aiutarvi a raggiungere i vostri obiettivi.';
 $_MODULE['<{mbo}prestashop>recommendedmodulesandservices_8e64cb6132c6f4db1e056bac48344ef9'] = '<p>Costruite una strategia basata sui dati e prendete decisioni più informate.<br>
                 Ecco una selezione di moduli partner,<strong> compatibili con il vostro negozio </strong>, per aiutarvi a raggiungere i vostri obiettivi.</p>';
-$_MODULE['<{mbo}prestashop>recommendedmodulesandservices_1f6f51f10d9e69ef3d2f293612f82dd1'] = 'Create esperienze memorabili e trasformate i visitatori in clienti.<br>
+$_MODULE['<{mbo}prestashop>recommendedmodulesandservices_085d4012e6e56f6f4ad58e03398dea79'] = 'Create esperienze memorabili e trasformate i visitatori in clienti.<br>
                 Ecco una selezione di moduli partner,<strong> compatibili con il vostro negozio </strong>, per aiutarvi a raggiungere i vostri obiettivi.';
-$_MODULE['<{mbo}prestashop>ps_mbo_dd1bd6bb90d44b5c7beeb4e4546181f6'] = 'Ecco una selezione di moduli partner,<strong> compatibili con il vostro negozio </strong>, per aiutarvi a raggiungere i vostri obiettivi.';
-$_MODULE['<{mbo}prestashop>ps_mbo_3891fd60e8d944a2de2cb2fca3a3c8bf'] = 'Altri moduli simili su PrestaShop Marketplace';
-$_MODULE['<{mbo}prestashop>ps_mbo_98dd43dfae05b11befe1f140e0ec787a'] = 'Installato';
-$_MODULE['<{mbo}prestashop>ps_mbo_ddd8eef6f86868a07f62b0e3810711f0'] = 'Non installato';
+$_MODULE['<{mbo}prestashop>recommendedmodulesandservices_7b98a7415047fae30575884f3aef3081'] = 'Ecco una selezione di moduli partner,<strong> compatibili con il vostro negozio </strong>, per aiutarvi a raggiungere i vostri obiettivi.';
+$_MODULE['<{mbo}prestashop>modulescatalog_3891fd60e8d944a2de2cb2fca3a3c8bf'] = 'Altri moduli simili su PrestaShop Marketplace';
+$_MODULE['<{mbo}prestashop>modulescatalog_98dd43dfae05b11befe1f140e0ec787a'] = 'Installato';
+$_MODULE['<{mbo}prestashop>modulescatalog_ddd8eef6f86868a07f62b0e3810711f0'] = 'Non installato';
 $_MODULE['<{mbo}prestashop>recommendedmodulesandservices_3891fd60e8d944a2de2cb2fca3a3c8bf'] = 'Altri moduli simili su PrestaShop Marketplace';
 $_MODULE['<{mbo}prestashop>recommendedmodulesandservices_98dd43dfae05b11befe1f140e0ec787a'] = 'Installato';
 $_MODULE['<{mbo}prestashop>recommendedmodulesandservices_ddd8eef6f86868a07f62b0e3810711f0'] = 'Non installato';
