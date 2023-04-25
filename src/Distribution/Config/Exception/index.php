c566639823abdb109e81fef9c8980096d6f15d45
Vincent Garcia <vgarcia.account@pm.me>
2023-04-25 11:50:20 +0200

    feat(All): :art: Apply index & header stamp in all files

--- src/Distribution/Config/Exception/index.php
+++ src/Distribution/Config/Exception/index.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Copyright since 2007 PrestaShop SA and Contributors
  * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
@@ -18,11 +17,12 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
  */
-$rootDir = getenv('_PS_ROOT_DIR_');
-if (!$rootDir) {
-    $rootDir = __DIR__ . '/../../';
-}
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
-require_once $rootDir . '/vendor/autoload.php';
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
 
-(new \Symfony\Component\Dotenv\Dotenv(true))->loadEnv(__DIR__ . '/.env');
+header('Location: ../');
+exit;
