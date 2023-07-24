9cfdf7aa46133c1ef7d15895390eb1fca45599e3
Ibrahima SOW <ibrahima.sow@prestashop.com>
2023-07-24 10:00:22 +0000

    Release 4.5.1

--- ps_mbo.php
+++ ps_mbo.php
@@ -53,7 +53,7 @@ class ps_mbo extends Module
     /**
      * @var string
      */
-    public const VERSION = '4.5.0';
+    public const VERSION = '4.5.1';
 
     public const CONTROLLERS_WITH_CONNECTION_TOOLBAR = [
         'AdminPsMboModule',
@@ -104,7 +104,7 @@ class ps_mbo extends Module
     public function __construct()
     {
         $this->name = 'ps_mbo';
-        $this->version = '4.5.0';
+        $this->version = '4.5.1';
         $this->author = 'PrestaShop';
         $this->tab = 'administration';
         $this->module_key = '6cad5414354fbef755c7df4ef1ab74eb';
