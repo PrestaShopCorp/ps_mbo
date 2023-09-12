2394fdf9f8ed2d3ce2f69bdbf444c5760973afec
Sullivan <sullivan.monteiro@prestashop.com>
2023-09-12 13:59:37 +0200

    fix: drag and drop on module catalogue

--- ps_mbo.php
+++ ps_mbo.php
@@ -459,6 +459,10 @@ class ps_mbo extends Module
         $this->context->controller->addJs($this->getPathUri() . 'views/js/recommended-modules.js?v=' . $this->version);
         $this->context->controller->addCSS($this->getPathUri() . 'views/css/catalog.css?v=' . $this->version, 'all', null, false);
 
+        if (Tools::getValue('controller') === "AdminPsMboModule") {
+            $this->context->controller->addJs($this->getPathUri() . 'views/js/upload_module_with_cdc.js?v=' . $this->version);
+        }
+
         if (
             $this->shouldAttachRecommendedModulesButton()
             || $this->shouldAttachRecommendedModulesAfterContent()
