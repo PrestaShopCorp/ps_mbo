489f017eab42aa890fe61c3eae9c4c206e380395
Ibrahima SOW <sowbiba@hotmail.com>
2023-09-13 11:38:11 +0000

    Release 2.3.3 (#621)
    * Add mbo_cdc_path if filterCategory is set (#617)
    
    * fix: bad margin/padding on 1.7 shop (#616)
    
    * [MBO-1265-1271] Drag&drop fix and module logo update on 2.3.x (#629)
    
    * fix: drag and drop on module catalogue
    
    * feat: change module logo
    
    * Update ps_mbo.php
    
    Co-authored-by: Ibrahima SOW <sowbiba@hotmail.com>
    
    * Update views/js/upload_module_with_cdc.js
    
    Co-authored-by: Ibrahima SOW <sowbiba@hotmail.com>
    
    ---------
    
    Co-authored-by: Ibrahima SOW <sowbiba@hotmail.com>
    
    ---------
    
    Co-authored-by: Rabin Julien <julien.rabin@prestashop.com>
    Co-authored-by: Sullivan <sullivan.monteiro@prestashop.com>

--- ps_mbo.php
+++ ps_mbo.php
@@ -124,6 +124,7 @@ class ps_mbo extends Module
         'AdminModulesUpdates',
         'AdminModulesManage',
         'AdminPsMboModule',
+        'AdminModulesCatalog',
     ];
 
     const HOOKS = [
@@ -458,6 +459,10 @@ class ps_mbo extends Module
         // has to be loaded in header to prevent flash of content
         $this->context->controller->addJs($this->getPathUri() . 'views/js/recommended-modules.js?v=' . $this->version);
         $this->context->controller->addCSS($this->getPathUri() . 'views/css/catalog.css?v=' . $this->version, 'all', null, false);
+
+        if (Tools::getValue('controller') === 'AdminPsMboModule') {
+            $this->context->controller->addJs($this->getPathUri() . 'views/js/upload_module_with_cdc.js?v=' . $this->version);
+        }
 
         if (
             $this->shouldAttachRecommendedModulesButton()
