44984c782f1295b6f91a87381ccf5964229d0afc
Sullivan <sullivan.monteiro@prestashop.com>
2023-10-25 15:21:36 +0200

    feat: add shop activity to context

--- src/Service/View/ContextBuilder.php
+++ src/Service/View/ContextBuilder.php
@@ -180,6 +180,7 @@ class ContextBuilder
             'module_catalog_url' => $this->router->generate('admin_mbo_catalog_module'),
             'theme_catalog_url' => $this->router->generate('admin_mbo_catalog_theme'),
             'php_version' => phpversion(),
+            'activity' => Configuration::get('PS_SHOP_ACTIVITY'),
         ];
     }
 
