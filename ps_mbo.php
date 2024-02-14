d11936eab41ef1831cb1d5fff7b5c59f0d5866c8
Ibrahima SOW <ibrahima.sow@prestashop.com>
2024-02-14 16:39:13 +0000

    Bump version to 4.10.1

--- ps_mbo.php
+++ ps_mbo.php
@@ -94,7 +94,7 @@ class ps_mbo extends Module
     public function __construct()
     {
         $this->name = 'ps_mbo';
-        $this->version = '4.10.0';
+        $this->version = '4.10.1';
         $this->author = 'PrestaShop';
         $this->tab = 'administration';
         $this->module_key = '6cad5414354fbef755c7df4ef1ab74eb';
