37dbcc4dad1fd502d8ba606f2faf85c06b3e3acf
Vincent Garcia <vgarcia.account@pm.me>
2024-05-20 16:18:36 +0200

    :bookmark: Bump version

--- ps_mbo.php
+++ ps_mbo.php
@@ -176,7 +176,7 @@ class ps_mbo extends Module
     public function __construct()
     {
         $this->name = 'ps_mbo';
-        $this->version = '3.1.2';
+        $this->version = '3.1.3';
         $this->author = 'PrestaShop';
         $this->tab = 'administration';
         $this->module_key = '6cad5414354fbef755c7df4ef1ab74eb';
