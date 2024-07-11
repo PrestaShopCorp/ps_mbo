e650e980c106a7cd067220211c7c910737e98c00
Vincent Garcia <vgarcia.account@pm.me>
2024-07-11 14:37:07 +0200

    :bookmark: Bump module version

--- ps_mbo.php
+++ ps_mbo.php
@@ -49,7 +49,7 @@ class ps_mbo extends Module
     /**
      * @var string
      */
-    public const VERSION = '4.11.3';
+    public const VERSION = '4.11.4';
 
     public const CONTROLLERS_WITH_CONNECTION_TOOLBAR = [
         'AdminModulesManage',
@@ -94,7 +94,7 @@ class ps_mbo extends Module
     public function __construct()
     {
         $this->name = 'ps_mbo';
-        $this->version = '4.11.3';
+        $this->version = '4.11.4';
         $this->author = 'PrestaShop';
         $this->tab = 'administration';
         $this->module_key = '6cad5414354fbef755c7df4ef1ab74eb';
