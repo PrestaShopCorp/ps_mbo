802b066e28b39a3fe9a2dcd7a1de357a09eab150
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2024-11-08 16:35:08 +0100

    Ensure module version const is synced but keep hard-coded value in constructor

--- ps_mbo.php
+++ ps_mbo.php
@@ -95,7 +95,11 @@ class ps_mbo extends Module
     public function __construct()
     {
         $this->name = 'ps_mbo';
-        $this->version = self::VERSION;
+        // This value must be hard-coded to respect Addons rules, so we must make sure that the const value is always synced with this one
+        $this->version = '5.0.0';
+        if ($this->version !== self::VERSION) {
+            throw new \PrestaShopException('The values from ps_mbo::$version and ps_mbo::VERSION must be identical');
+        }
         $this->author = 'PrestaShop';
         $this->tab = 'administration';
         $this->module_key = '6cad5414354fbef755c7df4ef1ab74eb';
