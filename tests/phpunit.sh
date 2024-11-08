75383906040ff56718dfe4a79e1dde91dd141a3d
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2024-11-08 17:57:15 +0100

    Upgrade phpunit to match Symfony version

--- tests/phpunit.sh
+++ tests/phpunit.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 PS_VERSION=$1
+
+# Clean container
+docker stop test-phpunit || true
 docker rm -f test-phpunit || true
 docker volume rm -f ps-volume || true
 
-docker run -tid --rm -v ps-volume:/var/www/html -v ${PWD}:/var/www/html/modules/ps_mbo --name test-phpunit prestashop/prestashop:$PS_VERSION
+docker run -tid --rm -v ps-volume:/var/www/html -v $PWD:/web/module --name test-phpunit prestashop/prestashop:$PS_VERSION
 
 until docker exec test-phpunit ls /var/www/html/vendor/autoload.php 2> /dev/null; do
   echo Waiting for docker initialization...
@@ -15,10 +18,10 @@ docker exec \
   -e PS_DOMAIN=localhost \
   -e PS_ENABLE_SSL=0 \
   -e PS_DEV_MODE=1 \
-  -w /var/www/html/modules/ps_mbo \
+  -e _PS_ROOT_DIR_=/var/www/html \
+  -w /web/module \
   test-phpunit \
   sh -c " \
     echo \"Testing module v\`cat config.xml | grep '<version>' | sed 's/^.*\[CDATA\[\(.*\)\]\].*/\1/'\`\n\" && \
     ./vendor/bin/phpunit -c ./tests/phpunit.xml \
-        "
-echo phpunit passed
+  "
