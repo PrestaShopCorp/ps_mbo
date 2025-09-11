2058105d2f79f5a6969581839dcdc47882b1d370
Jin JI <jin.ji@prestashop.com>
2025-09-11 15:23:42 +0200

    test: disable trailing_comma_in_multiline

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = new PrestaShop\CodingStandards\CsFixer\Config();
-$config->setRules(['@PhpCsFixer']);
+$config->setRules(['trailing_comma_in_multiline' => false]);
 /** @var \Symfony\Component\Finder\Finder $finder */
 $finder = $config->setUsingCache(true)->getFinder();
 $finder->in(__DIR__)->exclude('vendor');
