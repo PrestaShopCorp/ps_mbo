84a8989ca41cc71847bfb201f590eae4a2294ed1
Jin JI <jin.ji@prestashop.com>
2025-09-11 15:29:47 +0200

    test: php cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = new PrestaShop\CodingStandards\CsFixer\Config();
-$config->setRules(['trailing_comma_in_multiline' => false]);
+$config->setRules(['trailing_comma_in_multiline' => ['after_heredoc' => false]]);
 /** @var \Symfony\Component\Finder\Finder $finder */
 $finder = $config->setUsingCache(true)->getFinder();
 $finder->in(__DIR__)->exclude('vendor');
