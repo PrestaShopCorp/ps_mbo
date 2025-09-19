ac95499f91798a3dd8892c987b8ade1175107392
Vincent Garcia <vincent.garcia@prestashop.com>
2025-09-19 15:34:18 +0200

    :zap: Remove one more hook

--- src/Traits/UseHooks.php
+++ src/Traits/UseHooks.php
@@ -34,7 +34,6 @@ trait UseHooks
     use Hooks\UseDisplayDashboardTop;
     use Hooks\UseActionBeforeInstallModule;
     use Hooks\UseActionGetAlternativeSearchPanels;
-    use Hooks\UseDisplayAdminAfterHeader;
     use Hooks\UseActionListModules;
     use Hooks\UseDisplayEmptyModuleCategoryExtraMessage;
     use Hooks\UseActionBeforeUpgradeModule;
